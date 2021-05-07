7a5291fdfde9e3e261d17f153e6a518c02f7bc1b
kareemalsamman <morshed500@gmail.com>
2021-05-08 02:26:15 +0300

    sweetvibes

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
+
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 import orderRouter from './routes/order.js';
 import notificationRouter from './routes/notification.js';
 const app = express();
-dotenv.config();
+
 app.use(bodyParser.json({ limit: '10mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 app.use(cors());
@@ -19,12 +19,11 @@ app.use("/user", userRouter);
 app.use("/orders", orderRouter);
 app.use("/notification", notificationRouter);
 
-app.get('/', (req,res) => {
-  res.send('Welcome to sweetvibes');
-});
+
+const CONNECTION_URL = 'mongodb://root:toor@kareemalsamman-sweetvibes-production/sweetvibes';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
